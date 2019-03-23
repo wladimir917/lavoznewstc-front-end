cd3bf69acdb6bf3162cb5305432343ef3731f169
Fausto Wladimir Fraga <wladimir917@hotmail.com>
2019-03-23 16:07:17 -0400

    Initial setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,12 +9,12 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>Welcome to your new La Voz News site.</p>
+    <p>We are building something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/past-editions/">Past Editions</Link>
   </Layout>
 )
 
