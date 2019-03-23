8473ac92eb0395852c561d2cc7bffddbb0119eaf
Fausto Wladimir Fraga <wladimir917@hotmail.com>
2019-03-23 17:25:21 -0400

    Initial setup

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,11 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import Logo from "./logo";
 
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
       marginBottom: `1.45rem`,
     }}
   >
@@ -17,16 +17,30 @@ const Header = ({ siteTitle }) => (
       }}
     >
       <h1 style={{ margin: 0 }}>
+        <Logo
+          style={{
+            height: `10px`,
+          }}
+        />
         <Link
           to="/"
           style={{
-            color: `white`,
+            color: `black`,
             textDecoration: `none`,
           }}
         >
           {siteTitle}
         </Link>
       </h1>
+    </div>
+    <div
+      style={{
+        height: '1rem',
+        background: `gray`,
+        opacity: 0.1,
+        marginBottom: `1.45rem`,
+      }}
+    >
     </div>
   </header>
 )
