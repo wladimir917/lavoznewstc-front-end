03afc207b21fc7070ca72882a21493e58dc0cf60
Fausto Wladimir Fraga <wladimir917@hotmail.com>
2019-03-23 23:30:02 -0400

    basic layout

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,11 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import { css } from "@emotion/core"
+import { rhythm } from "../utils/typography"
+
 import Logo from "./logo";
+import logo from "../images/la-voz-logo.jpg"
 
 const ListLink = props => (
   <li style={{ display: `inline-block`, marginRight: `1rem` }}>
@@ -10,28 +14,31 @@ const ListLink = props => (
 )
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      marginBottom: `1.45rem`,
-    }}
-  >
+  <header>
     <div
       style={{
         margin: `0 auto`,
         maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        padding: `1rem 1.0875rem`
       }}
     >
-        <Logo/>
-        <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
-          <h3 style={{ display: `inline` }}>{siteTitle}</h3>
+      <div>
+        <img
+          src={logo}
+          alt="La Voz Logo"
+        />
+        <Link to="/" >
+          <h3>{siteTitle}</h3>
         </Link>
-        <ul style={{ listStyle: `none`, float: `right` }}>
+      </div>
+      <div style={{height: `50px`}}>
+        <ul style={{float: `right` }}>
           <ListLink to="/">Home</ListLink>
           <ListLink to="/about/">About</ListLink>
           <ListLink to="/contact/">Contact</ListLink>
           <ListLink to="/past-editions/">Past Editions</ListLink>
         </ul>
+      </div>
     </div>
     <div
       style={{
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,17 +2,29 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+import Banner from "../components/banner"
 import SEO from "../components/seo"
+import banner from "../images/banners/lawson.jpg"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new La Voz News site.</p>
-    <p>We are building something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div style={{ width: `60%`, display: `inline-block`, float: `left` }}>
+      <h1>Hi people</h1>
+      <p>Welcome to your new La Voz News site.</p>
+      <p>We are building something great.</p>
+    </div>
+    <div>
+      <img
+        src="https://images.pexels.com/photos/452738/pexels-photo-452738.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+        alt="Group of pandas eating bamboo"
+      />
+    </div>
+    <div>
+      <img
+        src={banner}
+        alt="Your add here"
+      />
     </div>
     <Link to="/past-editions/">Past Editions</Link>
   </Layout>
